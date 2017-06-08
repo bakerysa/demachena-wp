88764fcf57820368d9e41dec4e520dacd52c420a
Byron Polley <byronpolley@live.com>
2017-06-08 13:42:50 +0200

    removed incompatible theme

--- wp-content/themes/demachena/components/footer/site-info.php
+++
@@ -1,5 +0,0 @@
-<div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'demachena' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'demachena' ), 'WordPress' ); ?></a>
-	<span class="sep"> | </span>
-	<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'demachena' ), 'Dema-Chena', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
-</div><!-- .site-info -->
--- wp-content/themes/demachena/components/post/content-footer.php
+++
@@ -1,3 +0,0 @@
-	<footer class="entry-footer">
-		<?php demachena_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
