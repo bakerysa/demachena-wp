a7075f99acbd245d2d178bfbfac328dcbcc8a98c
Byron Polley <byronpolley@live.com>
2017-07-10 16:40:45 +0200

    mailchimp thank you redirect page

---
+++ wp-content/themes/demachena/template-thanks.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the homepage.
+ *
+ * This page template will display any functions hooked into the `homepage` action.
+ * By default this includes a variety of product displays and the page content itself. To change the order or toggle these components
+ * use the Homepage Control plugin.
+ * https://wordpress.org/plugins/homepage-control/
+ *
+ * Template name: Thanks
+ *
+ * @package storefront
+ */
+
+get_header('pink'); ?>
+
+	<div id="primary">
+		<main class="main" role="main">
+	    <div class="container center">
+				<div class="row text-center" style="padding-top: 4rem;">
+	        <h3>THANK YOU!</h3>
+	        <p>
+	          Welcome to the Dema Chena family.
+	        </p>
+	      </div>
+	    </div>
+	  </main>
+	</div><!-- #primary -->
+<?php
+get_footer('alt');
